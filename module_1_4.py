d7aca2471d04eeed1ea471980eb368a8ae0a6394
24122024 <arakelyantssasha@gmail.com>
2024-10-19 18:26:01 +0400

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,27 @@
+my_string = (input('Введите логин:'))
+login = my_string
+print(len(login))
+print(login. upper())
+print(login. lower())
+print(login.replace(' ', ''))
+print(login[0])
+print(login[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
